f4345d7b6fbfca71e283ee3c54de860fd9443ddb
Asim-Aqeel <163126547+Asim-Aqeel@users.noreply.github.com>
2024-03-19 01:26:44 +0500

    Add files via upload

---
+++ class4 operators/operat.js
@@ -0,0 +1,30 @@
+"use strict";
+// addition operator
+let num1 = 6;
+let num2 = 9;
+let cart = num1 + num2;
+console.log('adding', cart);
+// subtraction operator
+let sub1 = 56;
+let sub2 = 88;
+let total = sub1 - sub2;
+console.log('subtraction', total);
+// multiplication operator
+let mult1 = 7;
+let mult2 = 9;
+let multiply = mult1 * mult2;
+console.log('multipling:', multiply);
+//division operator
+let dev1 = 25;
+let dev2 = 5;
+let devision = dev1 / dev2;
+console.log('Devide', devision);
+//exponential operator
+let expo = 9;
+let power = expo ** 2;
+console.log('exponential', power);
+//percentage operator
+let apple = 76;
+let bags = 45;
+let remaining = apple % bags;
+console.log('remainder', remaining);
---
+++ class4 operators/operat.ts
@@ -0,0 +1,40 @@
+// addition operator
+let num1: number=6;
+let num2: number=9;
+let cart: number=num1 + num2;
+
+console.log('adding',cart);
+
+// subtraction operator
+
+let sub1: number=56;
+let sub2: number=88;
+let total: number=sub1 - sub2;
+console.log('subtraction',total);
+
+// multiplication operator
+
+let mult1:number=7;
+let mult2:number=9;
+let multiply:number=mult1*mult2;
+console.log('multipling:',multiply);
+
+//division operator
+
+let dev1:number=25;
+let dev2:number=5;
+let devision:number=dev1/dev2;
+console.log('Devide',devision,);
+
+//exponential operator
+
+let expo:number = 9;
+let power:number=expo**2;
+console.log('exponential',power);
+
+//percentage operator
+
+let apple:number=76;
+let bags:number=45;
+let remaining:number=apple % bags;
+console.log('remainder',remaining);
