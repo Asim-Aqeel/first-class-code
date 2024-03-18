176f65d93899f18c142826a43eb3b73b43459b3f
Asim-Aqeel <163126547+Asim-Aqeel@users.noreply.github.com>
2024-03-19 01:23:34 +0500

    Add files via upload

---
+++ class2 variables/varia.js
@@ -0,0 +1,26 @@
+"use strict";
+//variables Decleration let, const, var 
+/*
+ let box1 = ('Ghost');
+ const container = (70);
+ var chair = true;
+
+ console.log(box1);
+ console.log(container);
+ console.log(chair);
+   
+    //to chek data type
+ console.log(typeof chair);
+*/
+//variable type annotations string, number, boolean
+let box1 = ('Ghost');
+const age = (27);
+var variable = (true);
+console.log(age);
+//book example
+/*
+let favoriteColour = ("blue");
+console.log(favoriteColour);
+favoriteColour = ("green");
+console.log(favoriteColour);
+*/
---
+++ class2 variables/varia.ts
@@ -0,0 +1,42 @@
+    //variables Decleration let, const, var 
+
+ let boxe1 = ('Ghost');
+ const container = (70);
+ var chair = true;   
+
+ console.log(boxe1);
+ console.log(container);
+ console.log(chair);
+   
+    //to chek data type
+ console.log(typeof chair);
+
+
+    //variable type annotations string, number, boolean
+
+let box1: string = ('Ghost');
+const age:number = (27);
+var variable: boolean=(true);
+
+console.log(age);
+
+  //variables naming convention
+
+ let myName =('Ghost');   //camel case
+ let my_box=('Ghost');    //snake case
+ let MyChair=('Ghost');   //pascal case
+  
+
+
+    //book example
+/* 
+let favoriteColour = ("blue");
+console.log(favoriteColour);
+favoriteColour = ("green");
+console.log(favoriteColour); 
+*/
+
+
+
+
+
