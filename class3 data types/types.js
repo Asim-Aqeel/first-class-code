27c5a2f3f52c4c28c11f3a2323f560e8cfd0ad3d
Asim-Aqeel <163126547+Asim-Aqeel@users.noreply.github.com>
2024-03-19 01:25:33 +0500

    Add files via upload

---
+++ class3 data types/types.js
@@ -0,0 +1,29 @@
+"use strict";
+//string data type
+let myName = ('Ghost');
+console.log(myName);
+//number data type
+let age = 27;
+console.log(age);
+//boolean data type
+let isStudent = false;
+console.log(isStudent);
+console.log(9 === 9);
+//to chek type of any data type: "typeof"
+console.log(typeof isStudent);
+console.log('hello', 'world', 'this is a typescript');
+console.log('hello' + 'world', 'this is a type script code'); //concatenation
+//updation
+/*var myColour=('green');   //declaration
+myColour=('orange');     //update
+myColour=('peach');     //update
+var myColour=('red');  //again dacleration
+
+
+let mybox=('flowers');    //declare
+mybox=('rose');           //update
+let mybox=('apple');      //again decleration but let not support again decleration variable
+
+const yourbox=('bnana');   //declare
+yourbox=('guava');         //update nut const not support update or again declare
+*/
